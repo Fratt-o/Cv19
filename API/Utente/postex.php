dc342ab16e5d64f11aac9f0aa0ef66c6b68ed157
MrAllen27 <59915477+MrAllen27@users.noreply.github.com>
2020-03-29 11:14:00 +0200

    Eliminazione file superflui

--- API/Utente/postex.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head></head>
-    <body><form action="manuelboss.php" method="post">
-            Enter your name: <input type="text" name="nome" />
-<input type="submit" />
-</form>
-    </body>
-</html>
