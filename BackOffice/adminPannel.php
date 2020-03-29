743bfb61ccf5c3e56d9b808c142fac831425b772
Fratt-o <59869218+Fratt-o@users.noreply.github.com>
2020-03-29 11:18:25 +0200

    BackOffice 0.5
    Questo è l'UI del backoffice.

---
+++ BackOffice/adminPannel.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Admin Pannel</title>
+  </head>
+  <body>
+      <style>
+          
+          .pad{
+              
+              padding-top: 100px;
+              padding-bottom:100px;
+          }
+          headersetting{
+              width:100px;
+          }
+          .blocco{
+              border: 1px solid #007bff;
+          }
+          .bordoindex{
+             
+              border-right: 1px solid #007bff;
+          }
+          .bordoheader{
+              border-top: 1px solid #007bff;
+          }
+          .contenitore{
+              border: 1px solid #007bff;
+
+          }
+          .testo{
+              color : #007bff;
+          }
+      </style>
+      <div class="container contenitore">
+       
+            <nav class="navbar navbar-light ">
+                <span class="navbar-brand mb-0 h1 "style="color:#007bff;">
+                    
+                    <div class="dropdown">
+                        
+                        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Benvenuto, Fratto
+                        </button>
+                        
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item " href="index.php">Logout</a>
+                        </div>
+                    
+                    </div>
+                    
+                </span>
+            </nav>
+
+      
+            <div class="row bordoheader">
+          
+            
+           
+                <div class="col-sm-6 offset-sm-2  order-sm-2 pad ">
+                    
+                    <div class="testo">
+                        <a role="button" class="btn btn-outline-primary btn-lg btn-block" href="ModerationPannel.php">Modera Recensioni</a>
+                        <a role="button" class="btn btn-outline-primary btn-lg btn-block" href="StatisticPannel.php">Visualizza Statistiche</a>
+                    </div>
+              
+              
+           
+                </div>
+           
+                <div class="col-sm-2 col-xs-3 order-sm-1 bordoindex ">
+             
+
+          
+                </div>    
+            
+       
+            </div>
+      
+      
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
+
---
+++ BackOffice/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <title>BackOffice Home</title>
+  </head>
+  <body>
+      <style>
+          
+          .pad{
+              
+              padding-top: 100px;
+              padding-bottom:100px;
+          }
+          headersetting{
+              width:100px;
+          }
+          .blocco{
+              border: 1px solid #007bff;
+          }
+          .bordoindex{
+             
+              border-right: 1px solid #007bff;
+          }
+          .bordoheader{
+              border-top: 1px solid #007bff;
+          }
+          .contenitore{
+              border: 1px solid #007bff;
+
+          }
+
+      </style>
+      <div class="container contenitore">
+       
+           <nav class="navbar navbar-light ">
+               <span class="navbar-brand mb-0 h1 "style="color:#007bff;">Navbar</span>
+            </nav>
+
+      
+        <div class="row bordoheader">
+          
+            
+           
+          <div class="col-sm-6 offset-sm-2  order-sm-2 pad ">
+              
+              <div class=" " style="color:#007bff;">
+                   
+                  <div class="card-body ">
+                       
+                        <h5 class="card-title ">Benvenuto nel area di amministrazione di CV19</h5>
+                  
+                        <form>
+                        
+                            <div class="form-group">
+                                
+                                <label for="exampleInputEmail1">Indirizzo Email</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+                                <small id="emailHelp" class="form-text text-muted"></small>
+                            
+                            </div>
+                            
+                            <div class="form-group">
+                            
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control " id="exampleInputPassword1" required>
+                            
+                            </div>
+                            
+                             
+                            <button type="submit" class="btn btn-outline-primary">Accedi</button>
+                            <hr>
+                            <a href="adminPannel.php">
+                                Link Magico
+                            </a>
+                        </form>    
+                    
+                  </div>
+                  
+              </div>
+           
+          </div>
+           
+          <div class="col-sm-2 col-xs-3 order-sm-1 bordoindex ">
+             
+
+          
+          </div>    
+            
+       
+        </div>
+      
+      
+      </div>
+      
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
