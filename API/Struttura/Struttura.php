aaa25f45f1932868aea6236326dfc8c142092d44
MrAllen27 <59915477+MrAllen27@users.noreply.github.com>
2020-03-31 20:02:48 +0200

    Refactoring codice

--- API/Struttura/Struttura.php
+++ API/Struttura/Struttura.php
@@ -53,6 +53,40 @@ class Struttura
     function read($queryModel){
 		$page = $queryModel->pagination->page;
 		$pageSize = $queryModel->pagination->pageSize;
+		
+		$query = buildQuery($queryModel);
+		
+		$limit = $page*$pageSize. ',' . (($page*$pageSize + $pageSize)); 
+        $query .= "LIMIT $limit"; 
+		
+        // die($query);
+		$stmt = $this->conn->prepare($query);
+		
+		$stmt->execute();
+		return $stmt;
+    }
+    
+    function stampacaratteristiche(){
+    
+        $query = "SELECT a.idcaratteristica, a.nomecaratteristica FROM Caratteristiche a "
+                . "INNER JOIN StrutturaCaratteristiche b ON b.fkcaratteristica = a.idcaratteristica "
+                . "INNER JOIN Struttura c ON b.fkstruttura = c.idstruttura ";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+        
+    }
+	
+	function totalItemsPerFilters($queryModel) {
+		$query = buildQuery($queryModel);
+        // die($query);
+		$stmt = $this->conn->prepare($query);
+		
+		$stmt->execute();
+		return $stmt;
+	}
+	
+	function buildQuery($queryModel) {
 		$filter = $queryModel->filter;
 		$rating = $filter->rating;
 		$name = $filter->nome;
@@ -80,53 +114,10 @@ class Struttura
 			$where .= " AND s.mediavoto >= $rating ";
 		}
 		
-		$limit = $page*$pageSize. ',' . (($page*$pageSize + $pageSize)); 
-        $query = "SELECT * FROM Struttura s $join $where $groupBy $having LIMIT $limit"; 
-        // die($query);
-		$stmt = $this->conn->prepare($query);
-		
-		$stmt->execute();
-		return $stmt;
-    }
-    
-    function stampacaratteristiche(){
-    
-        $query = "SELECT a.idcaratteristica, a.nomecaratteristica FROM Caratteristiche a "
-                . "INNER JOIN StrutturaCaratteristiche b ON b.fkcaratteristica = a.idcaratteristica "
-                . "INNER JOIN Struttura c ON b.fkstruttura = c.idstruttura ";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
-        return $stmt;
-        
-    }
-	
-	function totalItemsPerFilters($queryModel) {
-		$filter = $queryModel->filter;
-		
-		$join = '';
-		$where = 'WHERE 1 = 1';
-		$groupBy = ' group by idstruttura ';
-		$having = '';
-		if(isset($filter->categoria)) {
-			$where .= " AND categoria = '$filter->categoria' ";
-		}
-		
-		if(isset($filter->caratteristiche) && count($filter->caratteristiche) > 0) {
-			$join .= " JOIN StrutturaCaratteristiche sc on s.idstruttura = sc.fkstruttura "; 
-			$caratteristiche = implode(',', $filter->caratteristiche);
-			$where .= " AND sc.fkcaratteristica in ($caratteristiche)";
-			$having = ' having count(idstruttura) = '.count($filter->caratteristiche);
-		}
-			
-		
-        $query = "SELECT * FROM Struttura s $join $where $groupBy $having"; 
-        // die($query);
-		$stmt = $this->conn->prepare($query);
-		
-		$stmt->execute();
-		return $stmt;
+        $query = "SELECT * FROM Struttura s $join $where $groupBy $having";
+		return $query;
 	}
     
     
-    }
+}
 ?>
